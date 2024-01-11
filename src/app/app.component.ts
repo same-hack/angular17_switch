8904cf97a7db3681fb5862e7ff1cf28d286ef8b9
same-hack <samehack.info@gmail.com>
2024-01-11 11:18:55 +0900

    fc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,14 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'angular17_switch';
+  // 信号の色
+  signalColor: string = '';
+
+  /**信号の色を設定する関数 */
+  setSignalColor(color: string) {
+    this.signalColor = color;
+  }
 }
